40f4a41d4c7a72ee251beb8b7bc7470460bda095
carrabre <40183833+carrabre@users.noreply.github.com>
2023-07-16 13:11:56 -0700

    updated website

--- app/embeddings/page.js
+++ app/embeddings/page.js
@@ -11,15 +11,19 @@ const Embeddings = () => {
                 <thead>
                     <tr>
                         <th>Timestamp</th>
+                        <th>Data</th>
                         <th>Title</th>
                         <th>File Size</th>
                         <th>Embeddings Model</th>
-                        <th>Download Link</th>
+                        <th>Download</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>7/13</td>
+                        <td>
+                            <a href="https://dumps.wikimedia.org/enwiki/20230701/">enWiki</a>
+                        </td>
                         <td>Wikipedia</td>
                         <td>10.31 MB</td>
                         <td>sentence-transformers/all-MiniLM-L6-v2</td>
@@ -29,6 +33,8 @@ const Embeddings = () => {
                     </tr>
                     <tr>
                         <td>...</td>
+                        <td> <a href= "https://commoncrawl.org/the-data/get-started/">CC Data </a> 
+                        </td>
                         <td>Common Crawl</td>
                         <td>...</td>
                         <td>...</td>
